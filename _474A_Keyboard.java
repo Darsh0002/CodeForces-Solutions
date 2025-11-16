eb3e5e448bf1d9cf37a72a3e968ab589c98de320
Darsh_Balar <darshbalar2005@gmail.com>
2025-11-16 12:35:33 +0530

    Added solution for 474A-Keyboard

---
+++ _474A_Keyboard.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class _474A_Keyboard {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        char dir = sc.next().charAt(0);
+        String str = sc.next();
+    }
+}
