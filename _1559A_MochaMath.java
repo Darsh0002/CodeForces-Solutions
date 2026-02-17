fa144f8d1d52d5bbc57e4bc66047c2ad3a976e2f
Darsh_Balar <darshbalar2005@gmail.com>
2026-02-17 20:05:49 +0530

    Problem 1559A - Bit Manipulation

---
+++ _1559A_MochaMath.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class _1559A_MochaMath {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int tc = sc.nextInt();
+        while (tc-- > 0) {
+            int n = sc.nextInt();
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = sc.nextInt();
+            }
+
+            long and = arr[0];
+            for (int i = 1; i < n; i++) {
+                and = and & arr[i];
+            }
+            System.out.println(and);
+        }
+    }
+}
