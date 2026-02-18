257ed7a2de8b6c90ebf53d03f3d46e2fa32a21f6
Darsh_Balar <darshbalar2005@gmail.com>
2026-02-18 20:48:11 +0530

    Problem 1537B - Geometry

---
+++ _1537B_BadBoy.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class _1537B_BadBoy {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int tc = sc.nextInt();
+        while (tc-- > 0) {
+            int n = sc.nextInt();
+            int m = sc.nextInt();
+            int i = sc.nextInt();
+            int j = sc.nextInt();
+
+            System.out.println(1 + " " + 1 + " " + n + " " + m);
+        }
+    }
+}
