74cd75276609c180eba1e59c27af37f0880e1240
Darsh_Balar <darshbalar2005@gmail.com>
2026-04-22 11:46:56 +0530

    Problem 2217B - Optimized

--- _2217B_FlipTheBit.java
+++ _2217B_FlipTheBit.java
@@ -16,24 +16,23 @@ public class _2217B_FlipTheBit {
             int x = a[p - 1];
 
             int ops = 0;
+            int flip = 0;
+
             int l = 0, r = n - 1; // l = [0,p-1] & r = [p-1,n-1]
             while (true) {
-                while (l < p && a[l] == x) l++;
+                while (l < p && (a[l] ^ flip) == x) l++;
                 if (l == p) l = p - 1;
 
-                while (r >= 0 && a[r] == x) r--;
+                while (r >= 0 && (a[r] ^ flip) == x) r--;
 
                 // all are equal to x
                 if (r < l) break;
 
                 if (r < p) r = p - 1;
 
-                // flip bits between [l,r]
+                // simulate flip
+                flip ^= 1;
                 ops++;
-                for (int i = l; i <= r; i++) {
-                    if (a[i] == 1) a[i] = 0;
-                    else a[i] = 1;
-                }
             }
             System.out.println(ops);
 
