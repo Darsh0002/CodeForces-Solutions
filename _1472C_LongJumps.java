37e6ff794efec7dcb7ada29a1d24125cd323f506
Darsh_Balar <darshbalar2005@gmail.com>
2026-06-02 12:23:36 +0530

    Problem 1472C - Dynamic Programming - Tabulation

--- _1472C_LongJumps.java
+++ _1472C_LongJumps.java
@@ -1,4 +1,3 @@
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class _1472C_LongJumps {
@@ -14,24 +13,21 @@ public class _1472C_LongJumps {
                 arr[i] = sc.nextInt();
             }
 
-            long[][] dp = new long[n][2];
-            for (long[] row : dp) Arrays.fill(row, -1);
+            long[][] dp = new long[n + 1][2];
 
-            System.out.println(solve(0, 0, arr, dp, n));
+            for (int i = n - 1; i >= 0; i--) {
+                for (int prev = 0; prev <= 1; prev++) {
+                    long notTake = 0;
+                    if (prev == 0)
+                        notTake = dp[i + 1][0];
+
+                    long take = arr[i] + ((i + arr[i] < n) ? dp[i + arr[i]][1] : 0);
+
+                    dp[i][prev] = Math.max(notTake, take);
+                }
+            }
+
+            System.out.println(dp[0][0]);
         }
-    }
-
-    private static long solve(int i, int prevTaken, int[] arr, long[][] dp, int n) {
-        if (i >= n) return 0;
-
-        if (dp[i][prevTaken] != -1) return dp[i][prevTaken];
-
-        long notTake = 0;
-        if (prevTaken == 0)
-            notTake = solve(i + 1, 0, arr, dp, n);
-
-        long take = arr[i] + solve(i + arr[i], 1, arr, dp, n);
-
-        return dp[i][prevTaken] = Math.max(notTake, take);
     }
 }
